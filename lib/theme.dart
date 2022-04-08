3423ae06423e341704f923a1bc37d4ca543d707b
Farhan <far.martiyansyah@gmail.com>
2022-04-08 11:33:50 +0700

    Update

---
+++ lib/theme.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+//TODO Warna
+Color whiteColor = Color(0xffFFFFFF);
+Color primaryColor = Color(0xff5843BE);
+Color twoColor = Color(0xfffFF9376);
+Color threeColor = Color(0xff82868E);
+Color fourColor = Color(0xff7A7E86);
+Color blackColor = Color(0xff000000);
+
+//TODO Font
+TextStyle whiteTextColor = GoogleFonts.poppins(color: whiteColor);
+TextStyle primaryTextColor = GoogleFonts.poppins(color: primaryColor);
+TextStyle twoTextColor = GoogleFonts.poppins(color: twoColor);
+TextStyle threeTextColor = GoogleFonts.poppins(color: threeColor);
+TextStyle fourTextColor = GoogleFonts.poppins(color: fourColor);
+TextStyle blackTextColor = GoogleFonts.poppins(color: blackColor);
+
+//TODO Ketebalan
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
